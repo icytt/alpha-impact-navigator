ab5ff8df5d584611e2a2d5f9b0eb69af93846830
Claire Lu <yl3666@cornell.edu>
2025-04-20 17:30:54 -0400

    Remove Vite-related files and cleanup project structure

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WCu7msKNul8flnX8TPxuUrem6oko/Hz97WM17/2uWz0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
